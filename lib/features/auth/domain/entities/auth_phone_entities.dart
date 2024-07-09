6a4ca01827b4246d52a6ea8103bb43a52e35af24
gomaa652 <faroukahmed17017@gmail.com>
2024-07-09 15:39:12 +0300

    Delete lib/features/auth directory

--- lib/features/auth/domain/entities/auth_phone_entities.dart
+++
@@ -1,9 +0,0 @@
-class AuthPhoneEntities {
-  final bool? statuss;
-  final String? messagee;
-  final String dataa;
-
-  AuthPhoneEntities(
-      {required this.statuss, required this.messagee, required this.dataa});
-}
-
